c8dddabc5ade3ed199f7db63b566713a37bc6cdc
VNK <nguyenkhoi8071@gmail.com>
2021-06-03 16:23:42 +0700

    Modified train file

--- train.py
+++ train.py
@@ -1,79 +1,58 @@
-import numpy as np
-import pandas as pd
-from sklearn import preprocessing
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.tree import DecisionTreeRegressor
-
-import seaborn as sns
-import matplotlib.pyplot as plt
-import config
-import model
+from src import config
+from src import model_train
 import os
 import argparse
 import joblib
-import json
-from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.model_selection import train_test_split
 
-from sklearn.linear_model import LogisticRegression
-#from lightgbm import LGBMClassifier
-from sklearn.ensemble import RandomForestClassifier
-#from xgboost import XGBClassifier
-from sklearn.ensemble import AdaBoostClassifier
-from sklearn.tree import DecisionTreeClassifier
-
-from sklearn.metrics import classification_report, roc_curve, auc, confusion_matrix
-from sklearn.metrics import f1_score, log_loss, roc_auc_score, recall_score, accuracy_score, precision_score
-
+from sklearn.metrics import classification_report, roc_auc_score, accuracy_score
+from src.dataset import preprocessing
+from imblearn.over_sampling import SMOTE
 import warnings
 warnings.filterwarnings("ignore")
 
-
 # Train
-def run(df, model):
-        df = pd.read_csv(config.TRAINING_FILE)
+def run(models):
+    train_dir = config.TRAINING_FILE
+    X, y = preprocessing(train_dir)
 
-        # Preprocessing
-        X = df.drop("stroke", axis= "columns").values
-        y = df.stroke
-        from sklearn.preprocessing import LabelEncoder
+    # Split data
+    X_train, X_test, y_train, y_test = train_test_split(X.values, y.values, test_size=0.3, random_state=0)
 
-        num_cols = X.select_dtypes(include = ['int64', 'float64']).columns.to_list()
-        cat_cols = X.select_dtypes(include = ['object']).columns.to_list()
-        def label_encoder(df):
-            for i in cat_cols:
-                le = LabelEncoder()
-                df[i] = le.fit_transform(df[i])
-            return df
+    
+    # Fetch model from model.py
+    #trainmodel = LogisticRegression()
+    trainmodel = model_train.models.values()
+ 
+    for models in trainmodel:
+    # Fit model on training data
+        models.fit(X_train,y_train)
 
-        sc = StandardScaler()
-        X[num_cols] = sc.fit_transform(X[num_cols])
-
-        # Label encoding
-        X = label_encoder(X)
-
-        # Split data
-        X_train, X_test, y_train, y_test = train_test_split(X.values, y.values, test_size=0.3, random_state=0)
-        cv = 10
-
-        # Fetch model from model.py
-        train_model = model.models[model]
-
-        # Fit model on training data
-        train_model.fit(X_train,y_train)
-
-        # Predictions
-        y_pred = train_model.predict(X_test)
-        y_prob = train_model.predict_proba(X_test)[:,1]
-        # Metric
+    # Predictions
+        y_pred = models.predict(X_test)
+        y_prob = models.predict_proba(X_test)[:,1]
+    
+    # Metric
+        print("------------------Model------------------")
+        print(models)
         accuracy = accuracy_score(y_test, y_pred)
-        print(f"Accuracy= {accuracy}")
+        print(f"Accuracy= {round(accuracy, 3)}")
+        print(f'ROC AUC score= {round(roc_auc_score(y_test, y_prob), 3)}')
         print(classification_report(y_test, y_pred))
-        print(f'ROC AUC score: {round(roc_auc_score(y_test, y_prob), 3)}')
-        # Save model
+    # Save model
         joblib.dump(
-            train_model,
-            os.path.makedirs(config.MODEL_OUTPUT, f"../models")
+            models,
+            os.path.join(config.MODEL_OUTPUT, f"../models")
         )
 
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--model', type=str)
+    
+    args = parser.parse_args()
+
+    run(models=args.model)
 # Outputs
