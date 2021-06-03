74b309e96c0c5c497815e2d83f745cc8c990f7ae
VNK <nguyenkhoi8071@gmail.com>
2021-06-03 13:33:34 +0700

    Modified dataset file

--- src/dataset.py
+++ src/dataset.py
@@ -15,20 +15,22 @@ folder_dir = "./data"
 def df():
     for filename in glob(os.path.join(folder_dir, "*.zip")):
         df = pd.read_csv(filename)
-        def data_handlemissing(df):
-            DT_bmi_pipe = Pipeline(steps=[('scale',StandardScaler()),
+    return df
+        
+def data_handlemissing(df):
+    DT_bmi_pipe = Pipeline(steps=[('scale',StandardScaler()),
                                 ('dtr',DecisionTreeRegressor(random_state=42))])
-            X_BMI = df[['age','gender','bmi']].copy()
-            X_BMI.gender = X_BMI.gender.replace({'Male':0,'Female':1,'Other':-1}).astype(np.uint8)
+    X_BMI = df[['age','gender','bmi']].copy()
+    X_BMI.gender = X_BMI.gender.replace({'Male':0,'Female':1,'Other':-1}).astype(np.uint8)
 
-            Missing = X_BMI[X_BMI.bmi.isna()]
-            X_BMI = X_BMI[~X_BMI.bmi.isna()]
-            Y_BMI = X_BMI.pop('bmi')
-            DT_bmi_pipe.fit(X_BMI,Y_BMI)
-            predicted_bmi = pd.Series(DT_bmi_pipe.predict(Missing[['age','gender']]),index=Missing.index)
-            df.loc[Missing.index,'bmi'] = predicted_bmi
-            df.drop(df[df['gender'] == "Other"].index, inplace = True)
-        return df
+    Missing = X_BMI[X_BMI.bmi.isna()]
+    X_BMI = X_BMI[~X_BMI.bmi.isna()]
+    Y_BMI = X_BMI.pop('bmi')
+    DT_bmi_pipe.fit(X_BMI,Y_BMI)
+    predicted_bmi = pd.Series(DT_bmi_pipe.predict(Missing[['age','gender']]),index=Missing.index)
+    df.loc[Missing.index,'bmi'] = predicted_bmi
+    df.drop(df[df['gender'] == "Other"].index, inplace = True)
+    return df
 
 
 
