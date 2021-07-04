3a48ccad094c6da3d4046e0ae03d7837093676ad
VNK <nguyenkhoi8071@gmail.com>
2021-07-04 14:56:59 +0700

    Test single sample

--- test.py
+++ test.py
@@ -1,9 +1,13 @@
+from src.model_train import save_models
 from src.StrokeDetector import StrokeDetector
+import numpy as np
 
-MODEL_PATH = '<dir to your model file>'
-PARAM_PATH = '<dir to your model file>'
-x = inputfromuser()
+MODEL_PATH = './models/' + save_models["Logistic Regression"]
+
+patient_input = np.array([1,20,1,1,1,150,30,0])
 
 detector = StrokeDetector(modeltype=MODEL_PATH)
 
-out = detector.predict(x)
+out = detector.predict(patient_input)
+
+print("Output predict :", out)
