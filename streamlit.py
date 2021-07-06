cf5252a094977f018f979c56c995c3f0a757fc6c
VNK <nguyenkhoi8071@gmail.com>
2021-07-06 14:23:59 +0700

    Change class StrokeDetector into src

--- streamlit.py
+++ streamlit.py
@@ -1,11 +1,8 @@
-import joblib
 import streamlit as st
-from src import model_train
-from src import config
 import numpy as np
 import pandas as pd
 from src.dataset import label_encoder
-from src.param import mean, std
+from StrokeDetector import StrokeDectector
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -20,38 +17,6 @@ st.write("Please set your profile to predict")
 classifier_model = st.sidebar.selectbox("Select model", ("Logistic Regression",\
                         "LightGBM Classifier", "Random Forest", "XGB Classifier",\
                         "Adaboosting Classifier", "Decision Tree"))
-
-class StrokeDectector:
-    def __init__(self, classifier_model):
-        self.model = joblib.load(config.MODEL_OUTPUT + model_train.save_models[classifier_model])
-        self.mean = mean
-        self.std = std 
-    
-
-    def user_predict(self, patient):
-        '''
-        Input: Patient status with label encoding
-
-        Return: Type ndarray of patient status after using Standardization method
-        '''
-        patient_predict = (patient - self.mean) / self.std
-        return patient_predict
-
-
-    def predict(self, patient):
-        '''
-        Input: Paitent status 
-
-        Return: Predict stroke or no stroke
-        '''
-        patient_predict = self.user_predict(patient)
-        output = self.model.predict(patient_predict)
-        
-        if output == 0:
-            output = "No stroke"
-        else:
-            output = "Stroke" 
-        return output 
 
 
 def input_user():
